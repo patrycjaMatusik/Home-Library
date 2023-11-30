0496cedec3bddca77a7911864674dbf914777002
Patrycja Matusik <pmatusik11@gmail.com>
2023-11-30 21:51:33 +0100

    Delete user

---
+++ HomeLibAPI/Exceptions/NotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeLibAPI.Exceptions
+{
+    public class NotFoundException
+    {
+    }
+}
